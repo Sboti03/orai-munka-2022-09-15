aa5915d5058fce7e9ce0515a337320cd4b095c64
avpboy <somogyibotond03@gmail.com>
2022-09-15 12:09:46 +0200

    test.js + start

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import {add, average} from './lista.js'
-
-add(12)
-add(12)
-
-console.log(average())
---
+++ src/test.js
@@ -0,0 +1,14 @@
+import {add, average} from './lista.js'
+
+add(1)
+console.log(average())
+add(10)
+console.log(average())
+add(23)
+console.log(average())
+
+add(-12)
+console.log(average())
+
+add(22)
+console.log(average())
