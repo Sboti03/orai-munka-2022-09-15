af61022e1a653bbe31d8d2361c843e9b4ae6aa41
avpboy <somogyibotond03@gmail.com>
2022-09-15 12:22:14 +0200

    html

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import {add, average} from './lista.js'
+
+function numberAdd() {
+    add(document.getElementById('szam_mezo').value)
+    console.log(document.getElementById('szam_mezo').value)
+    alert(average())
+}
+
+document.getElementById('enter').addEventListener('click', numberAdd)
