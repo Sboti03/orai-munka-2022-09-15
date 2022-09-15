15040a96feeb3f02ae5758937a231e266637379c
avpboy <somogyibotond03@gmail.com>
2022-09-15 12:06:31 +0200

    lista.js

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import {add, average} from './lista.js'
+
+add(12)
+add(12)
+
+console.log(average())
---
+++ src/lista.js
@@ -0,0 +1,20 @@
+let lista = []
+
+
+function add(number) {
+    if(number > 0){
+        lista.push(number)
+    } else {
+        console.log("Hiba")
+    }
+}
+
+function average(){
+    let count = 0
+    for(let i = 0; i < lista.length; i++) {
+        count += lista[i]
+    }
+    return count / lista.length
+}
+
+export {average, add}
